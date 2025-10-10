03a96d3bcc14fe0254f7b55c13a08f4f3b2c4f43
ouennoughi aya <ouennoughiaya@gmail.com>
2025-10-10 20:13:45 +0100

    Auto update

---
+++ theCtry.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+    int i;
+    printf("enter a number :");
+    scanf("%d",&i);
+    printf("the double of %d is %d",i,i*2);
+
+    return 0;
+}
